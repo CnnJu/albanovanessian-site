1e58ae7638c69ff915f5ba69b6c9242f15e86008
CnnJu <jucanon90@gmail.com>
2024-12-28 21:05:00 +0100

    tidy code

--- cominginout.js
+++ cominginout.js
@@ -22,17 +22,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (container) {
         uniqueImages.forEach(src => {
-            const imgWrapper = document.createElement('div');
-
             const img = document.createElement('img');
             img.src = src;
-
-            imgWrapper.appendChild(img); 
-            container.appendChild(imgWrapper); 
+            container.appendChild(img);
         });
     } else {
         console.log("No .project-img element found on this page.");
     }
+
+    // news image array
+    const entries = [
+        {
+            src: 'img/project/breakintwo/IMG_7834_2_PNG.webp',
+            h1: 'Title 1',
+            h2: 'Subtitle 1',
+            h3: '',
+            h4: '',
+            h5: 'Additional Info 1'
+        },
+        {
+            src: 'img/project/breakintwo/IMG_7827_PNG.webp',
+            h1: 'Title 2',
+            h2: '',
+            h3: 'Subtitle 2',
+            h4: '',
+            h5: ''
+        },
+        // Add other entries as needed
+    ];
 
     // Logos array for funding organizations
     const logos = [
--- rosasvartstation.js
+++ rosasvartstation.js
@@ -42,4 +42,25 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.log("No .logofunding element found on this page.");
     }
+
+    // news image array
+    const entries = [
+        {
+            src: 'img/project/breakintwo/IMG_7834_2_PNG.webp',
+            h1: 'Title 1',
+            h2: 'Subtitle 1',
+            h3: '',
+            h4: '',
+            h5: 'Additional Info 1'
+        },
+        {
+            src: 'img/project/breakintwo/IMG_7827_PNG.webp',
+            h1: 'Title 2',
+            h2: '',
+            h3: 'Subtitle 2',
+            h4: '',
+            h5: ''
+        },
+        // Add other entries as needed
+    ];
 });
