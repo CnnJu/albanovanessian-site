975984df4c3c8196d726b2b9bb2a08bfe7fdc3de
CnnJu <jucanon90@gmail.com>
2024-12-29 12:20:50 +0100

    js rosa svart

--- rosasvartstation.js
+++ rosasvartstation.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Project image array
     const images = [
-        'img/project/queerhub/1imgarray.jpg',
-        'img/project/queerhub/2imgarray.jpg',
-        'img/project/queerhub/3imgarray.jpg',
-        'img/project/queerhub/4imgarray.jpg',
-        'img/project/queerhub/5imgarray.jpg',
-        'img/project/queerhub/6imgarray.jpg',
-        'img/project/queerhub/7imgarray.jpg',
-        'img/project/queerhub/8imgarray.jpg'
+        'img/project/rosasvartstation/rosasvart2.JPG',
+        'img/project/rosasvartstation/rosasvart3.jpg',
+        'img/project/rosasvartstation/rosasvart4.jpg',
+        'img/project/rosasvartstation/rosasvart5.jpg'
     ];
 
     const uniqueImages = [...new Set(images)]; // Ensure all images are unique
@@ -18,49 +14,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (container) {
         uniqueImages.forEach(src => {
-            const imgWrapper = document.createElement('div');
+            const imageContainer = document.createElement('div');
 
             const img = document.createElement('img');
+            imageContainer.appendChild(img); 
             img.src = src;
-
-            imgWrapper.appendChild(img); 
-            container.appendChild(imgWrapper); 
+            container.appendChild(imageContainer); 
         });
     } else {
-        console.log("No .project-img element found on this page.");
+        console.error("No .project-img element found on this page.");
     }
       
+    const logos = [
+        { src: 'img/logo1.png', alt: 'Logo 1' },
+        { src: 'img/logo2.png', alt: 'Logo 2' },
+        { src: 'img/logo3.png', alt: 'Logo 3' }
+    ];
+
     const logoContainer = document.querySelector('.logofunding');
+
     if (logoContainer) {
         logos.forEach(logo => {
             const img = document.createElement('img');
-            img.src = logo;
-            img.alt = 'Funder Logo';
+            img.src = logo.src;
+            img.alt = logo.alt;
             img.classList.add('funder-logo');
             logoContainer.appendChild(img);
         });
     } else {
-        console.log("No .logofunding element found on this page.");
+        console.error("No .logofunding element found on this page.");
     }
-
-    // news image array
-    const entries = [
-        {
-            src: 'img/project/breakintwo/IMG_7834_2_PNG.webp',
-            h1: 'Title 1',
-            h2: 'Subtitle 1',
-            h3: '',
-            h4: '',
-            h5: 'Additional Info 1'
-        },
-        {
-            src: 'img/project/breakintwo/IMG_7827_PNG.webp',
-            h1: 'Title 2',
-            h2: '',
-            h3: 'Subtitle 2',
-            h4: '',
-            h5: ''
-        },
-        // Add other entries as needed
-    ];
 });
