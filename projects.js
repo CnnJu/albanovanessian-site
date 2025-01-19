72d841ec3177e65c99d5738323aec87e0982ff7c
CnnJu <jucanon90@gmail.com>
2025-01-19 15:11:20 +0100

    refactor image grid layout and project display; enhance styles for improved visibility and responsiveness

--- projects.js
+++ projects.js
@@ -3,94 +3,81 @@ const projects = [
     link: "queerhub.html",
     title: "Queerhub",
     year: "2022",
-    image: "img/project/projects/queerhub",
+    image: "img/project/940px_queerhub.png",
     description: "En Lire Plus",
-  },
-  {
-    link: "break-in-two.html",
-    title: "break.in.two",
-    year: "2019",
-    image: "img/project/projects/breakintwo",
-    description: "En Lire Plus",
-  },
-  {
-    link: "comingoutsolo.html",
-    title: "COMING in out (solo)",
-    year: "2019",
-    image: "img/project/projects/comingoutsolo",
-    description: "En Lire Plus",
-  },
-  {
-    link: "cominginout.html",
-    title: "COMING in out (vidéo installation)",
-    year: "2019",
-    image: "img/project/projects/comingoutvideo",
-    description: "En Lire Plus",
-  },
-  {
-    link: "rosasvartstation.html",
-    title: "ROSA.SVART.STATION",
-    year: "2021",
-    image: "img/project/projects/rosasvart",
-    description: "En Lire Plus",
+    fontClass: "queerhub-font"
   },
   {
     link: "inthroughwater.html",
     title: "in.through.WATER",
     year: "2022",
-    image: "img/project/projects/inthrough",
-    video: "https://www.youtube.com/embed/HxSUWAqFGuI?si=Msy_7Q46oWm64jsO",
+    image: "img/project/470px_inthroughwater.gif",
     description: "En Lire Plus",
+    fontClass: "inthroughwater-font"
   },
   {
-    link: "griefhub.html",
-    title: "GriefHub",
-    year: "2020",
-    image: "img/project/projects/GriefHub",
+    link: "inthroughwater.html",
+    title: "in.through.WATER",
+    year: "2022",
+    image: "img/project/470px_inthroughwater.gif",
     description: "En Lire Plus",
+    fontClass: "inthroughwater-font"
   },
+  {
+    link: "breakintwo.html",
+    title: "break.in.two",
+    year: "2019",
+    image: "img/project/940px_xdates.png",
+    description: "En Lire Plus",
+    fontClass: "breakintwo-font"
+  },
+  {
+    link: "rosasvartstation.html",
+    title: "ROSA.SVART.STATION",
+    year: "2021",
+    image: "img/project/1410px_rosasvartstation.png",
+    description: "En Lire Plus",
+    fontClass: "rosasvartstation-font"
+  },
+  {
+    link: "cominginoutsolo.html",
+    title: "COMING in out (solo)",
+    year: "2019",
+    image: "img/project/940px_cominginoutsolo.png",
+    description: "En Lire Plus",
+    fontClass: "cominginoutsolo-font"
+  },
+  {
+    link: "comingoutvideo.html",
+    title: "COMING in out (vidéo installation)",
+    year: "2019",
+    image: "img/project/470px_comingoutvideo.png",
+    description: "En Lire Plus",
+    fontClass: "comingoutvideo-font"
+  },
+  {
+    link: "breakintwo.html",
+    title: "break.in.two",
+    year: "2019",
+    image: "img/project/470px_breakintwo.png",
+    description: "En Lire Plus",
+    fontClass: "breakintwo-font"
+  }
 ];
 
 const projectsGrid = document.getElementById("projects-grid");
 
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * 3) + 1;
-};
-
 projects.forEach((project) => {
   const projectDiv = document.createElement("div");
   projectDiv.classList.add("project");
-  projectDiv.classList.add("box");
-
-  let height;
-  let width;
-  let img;
-
-  const number = getRandomNumber();
-  if (number === 1) {
-    height = 250;
-    width = 250;
-    img = project.image + "_square_500x500.jpg";
-    projectDiv.classList.add("square");
-  } else if (number === 2) {
-    height = 500;
-    width = 250;
-    img = project.image + "_tall_500x1000.jpg";
-    projectDiv.classList.add("tall");
-  } else {
-    height = 250;
-    width = 500;
-    img = project.image + "_wide_1000x500.jpg";
-    projectDiv.classList.add("wide");
-  }
 
   const projectContent = `
       <a href="${project.link}" class="project-link">
-        <div class="image-container" style="width: ${width}px; height: ${height}px;">
-          ${`<img src="${img}" alt="${project.title}" class="project-image" style="width: 100%; height: 100%;">`}
+        <div class="image-container">
+          <img src="${project.image}" alt="${project.title}" class="project-image">
         </div>
         <div class="project-img-overlay">
-          <h1 class="projects-page-title">${project.title}</h1>
+          <h1 class="projects-page-title ${project.fontClass}">${project.title}</h1>
           <h5 class="year">${project.year}</h5>
           <h5 class="read-more">${project.description}</h5>
         </div>
